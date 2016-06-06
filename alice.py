d44ee6e8f692df239a4262a2f26184666612eb65
Jeremy Benz <jeremy.benz@gmail.com>
2016-06-05 23:37:00 -0700

    Finished!
    This program is the antithesis of DRY... But I got it working!

--- alice.py
+++ alice.py
@@ -1,49 +1,48 @@
 # coding: utf-8
 
 import time
-import os
+import console
 
-#Intial Values
-inventory = []
-x_pos = 3
-y_pos = 1
-current_room = "ent"
-jab = "alive"
+def cls():
+	os.system('cls')
 
-#Movement Functions
-def north():
-	y_pos += 1
-def east():
-	x_pos += 1
-def south():
-	y_pos -= 1
-def west():
-	x_pos -= 1
-def through():
-	x_pos -= 1
+#Nope! (part of an old sequence of functions that I didn't feel like changing)
 def nope():
-	print "You can't go that way, Alice!\n"
+	print "You do that, Alice!\n"
 	
 #Room Descriptions
-ent_desc = "You stand at the entrance of the Jabberwok's abode. It's quite fancy for the home of a ravening beast. Two small side tables sprout lovely potted ferns, and the teal patterned wallpaper coordinates pleasingly with the white tile flooring.\n\n"
-foy_desc = "The Jabberwok's foyer is massive and ornate. A collection of beanie babies in a crystal cabinet attracts your attention.\n\n"
-clo_desc = "This large walk-in closet is piled high with dusty old newpapers and expired coupons. Looks like the Jabberwork is a bit of a hoarder.\n\n"
-sit_desc = "This room is full of overstuffed chairs, each with the bottom cut out to accomodate the Jabberwok's tail. The fireplace is banked and lit, bathing the room in a warm, friendly glow.\n\n"
-gal_desc = "Artful paintings adorn the walls of this well-kept gallery. One large oil painting draws your eye. It depicts a young woman in a blue dress brandishing a deadly-looking sword. She is deftly challenging a scaly, toothsome beast.\n\n"
-hal_desc = "This hallway is covered in scorch marks and corn dog sticks. This part of the abode is not nearly as neat as the rest of the house. You must be getting close to the Jabberwok's den. A massive oak and iron-shod door stands at the end of the hallway.\n\n"
-den_desc = "The center of the room is dominated by large cauldron, currently occupied by a weeping Mad Hatter. The Jabberwok is adding some finely diced onions to the pot, when the beast notices your trespass unto its domain. It pulls a chain suspended from the high ceiling, and a portculis slams down, blocking your exit!\n\n"
+ent_desc = "You stand at the entrance of the Jabberwok's abode. It's quite fancy for the home of a ravening beast. Two small side tables sprout lovely potted ferns, and the teal patterned wallpaper coordinates pleasingly with the white tile flooring.\n"
+foy_desc = "The Jabberwok's foyer is massive and ornate. A collection of beanie babies in a crystal cabinet attracts your attention.\n"
+clo_desc = "This large walk-in closet is piled high with dusty old newpapers and expired coupons. Looks like the Jabberwork is a bit of a hoarder.\n"
+sit_desc = "This room is full of overstuffed chairs, each with the bottom cut out to accomodate the Jabberwok's tail. The fireplace is banked and lit, bathing the room in a warm, friendly glow.\n"
+gal_desc = "Artful paintings adorn the walls of this well-kept gallery. One large oil painting draws your eye. It depicts a young woman in a blue dress brandishing a deadly-looking sword. She is deftly challenging a scaly, toothsome beast.\n"
+hal_desc = "This hallway is covered in scorch marks and corn dog sticks. This part of the abode is not nearly as neat as the rest of the house. You must be getting close to the Jabberwok's den. A massive oak and iron-shod door stands at the end of the hallway.\n"
+den_desc = "The center of the room is dominated by large cauldron, currently occupied by a weeping Mad Hatter. The Jabberwok is adding some finely diced onions to the pot, when the beast notices your trespass unto its domain. It pulls a chain suspended from the high ceiling, and a portculis slams down, blocking your exit!\n"
 
 #Encounters
-enc_start = "You stare at the empty seat at the head of the table. 'Not like him at all, missing his very own unbirthday,' the March Hare snorts.\n\n'He's late for a very important date!' the White Rabbit yells into his pocket watch.\n\nYou, of all people, know of the Mad Hatter's eccentricities; but this is pushing it, even for him. The tea grows cold as dark clouds occlude the sun, darkening the clearing. The Doormouse is getting very anxious. You make sure the pot of jam is well-stocked in case she needs calming down.\n\nA white feline grin materializes over the tea service, followed by the rest of the Cheshire Cat.\n\n'Our flamboyant friend has been snatched up by the Jabberwok. The foul beast has taken him back to its abode; to make a meal of him, I'm almost sorry to say,' the Cat purrs.\n\nYour friend is in grave danger!\n\nWithout a second thought, you bolt from the table and into the Tolgey Woods. You jump into the third mushroom ring, and find yourself at the front door of the Jabberwok's homestead. You fling open the front door and advance northward into the lair of the most fearsome beast in all of Wonderland.\n\n"
+enc_start = "You stare at the empty seat at the head of the table. 'Not like him at all, missing his very own unbirthday,' the March Hare snorts.\n\n'He's late for a very important date!' the White Rabbit yells into his pocket watch.\n\nYou, of all people, know of the Mad Hatter's eccentricities; but this is pushing it, even for him. The tea grows cold as clouds occlude the sun, darkening the clearing. The Doormouse is getting very anxious. You make sure the pot of jam is well-stocked in case she needs calming down.\n\nA white feline grin materializes over the tea service, followed by the rest of the Cheshire Cat.\n\n'Our flamboyant friend has been snatched up by the Jabberwok. The foul beast has taken him back to its abode. To make a meal of him, I'm almost sorry to say,' the Cat purrs.\n\nYour friend is in grave danger!\n\nWithout a second thought, you bolt from the table and into the Tolgey Woods. You jump into the third mushroom ring, and find yourself at the front door of the Jabberwok's homestead. You fling open the front door and advance northward into the lair of the most fearsome beast in all of Wonderland.\n\n"
 enc_cannot = "You cannot leave the Jabberwok's domain while your friend is in grave peril!\n\n"
 enc_sword = "You look closely at the painting, and see that it's not sitting flush with the wall. You gingerly pull the bottom of the artwork away from the wall, and discover a gleaming sword hanging from two nails hammered into the plaster. You take the vorpal sword and return the painting to its place.\n\n"
 enc_key = "You shuffle through the odd bits of paperwork, vastly increasing the amount of airborn dust in the closet. You are about to end your search, when you spy the glint of metal amongst the detritus piled in the corner. You open an old Penny Saver leaflet to find an ornate golden key, which you place into your front pocket.\n\n"
 enc_nothing = "Your search reveals nothing.\n\n"
+enc_found = "You've already found the hidden item in this room, Alice!\n\n"
 enc_locked = "This door is locked.\n\n"
 enc_door = "You pull the golden key from your front pocket, and slide it into the keyhole on the massive door. It opens inward, and you journey through.\n\n"
 enc_boss = "'Alice, my dear!' cries the Mad Hatter, 'Flee before you join me in this wretched monster's stew!' The Jabberwok explodes into motion, rearing up for a strike.\n\n"
-enc_defeat = "With nothing to fight the Jabberwok, you remember the old adage 'Music can soothe the savage beast.' You being to sing an old song about a baby on a treetop in a lovely contralto. The Jabberwok stops instantly and starts swaying languidly to your song. As soon as the 'bough breaks,' however; the beast shakes and strikes! You feel terrible teeth piercing your throat!\n\n You awaken to Dinah, your kitten bumping into your right hand seeking its caresses. 'Alice! It's time to finish your history lesson!' your sister calls from the Solarium. You sigh, scoop up Dinah, and head back indoors."
-enc_victory = "You bravely brandish the vorpal sword in the face of your adversary. The Jabberwok darts in for a killing blow, and the blade goes 'SNICKER SNACK!' The Jabberwok's grusome head tumbles from its gory shoulders.\n\n 'Hooray!' cries the Mad Hatter. You help your friend from the cauldron (luckily the water was just beginning to heat up), and walk with him outside back into the Tolgey Woods. Soon, you all are gathered at the table in the clearing.\n\n 'About time,' sighs the White Rabbit as the Doormouse pours the tea.\n\n"
+enc_defeat = "With nothing to fight the Jabberwok, you remember the old adage 'music soothes the savage beast.' You sing, in your lovely contralto, an old song about a baby on a treetop. The Jabberwok stops and sways languidly to your song. As soon as the 'bough breaks,' however; the beast shakes and strikes! You feel terrible teeth piercing your throat!\n\nYou awaken to Dinah, your kitten bumping into your right hand seeking its caresses. 'Alice! It's time to finish your history lesson,' your sister calls from the Solarium. You sigh, scoop up Dinah, and head back indoors.\n\nSorry, Alice. It's time to become an adult."
+enc_victory = "You bravely brandish the vorpal sword in the face of your adversary. The Jabberwok darts in for a killing blow, and the blade goes 'SNICKER SNACK!' The Jabberwok's savage head tumbles from its scaly shoulders.\n\n'Hooray!' cries the Mad Hatter. You help your friend from the cauldron (luckily the water was just beginning to heat up), and pull the chain, raising the portcullis.\n\nYou and the Mad Hatter walk outside and back into the Tolgey Woods. Soon, you are all gathered at the table in the clearing.\n\n'About time,' sighs the White Rabbit as the Doormouse pours the tea.\n\nNice work, Alice!\n\n"
+
+#Hero Class
+class Hero(object):
+	def __init__ (self, x_pos, y_pos, current_room, have_key, have_sword, jab):
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.current_room = current_room
+		self.have_key = have_key
+		self.have_sword = have_sword
+		self.jab = jab
+		
+alice = Hero(3, 1, "ent", "no", "no", "alive")
 
 #Room Class
 class Room(object):
@@ -58,26 +57,33 @@ class Room(object):
 		self.t_exit = t_exit
 		
 	#Entering the Room
-	def room_enter(self):
+	def room_enter(self, alice):
 		print (self.name) + ":\n" + (self.desc)
 		
 	#Searching the Room
-	def room_search(self):
+	def room_search(self, alice):
 		if (self.item != "nothing"):
-			if (current_room == "gal"):
-				inventory.append("a vorpal sword")
-				gal.item = "nothing"
+			if (current_room == "gal" and alice.have_sword == "no"):
+				alice.have_sword = "yes"
 				print(enc_sword)
-			else:
-				inventory.append("a golden key")
-				clo.item = "nothing"
+			elif (current_room == "gal" and alice.have_sword == "yes"):
+				print(enc_found)
+			elif (current_room == "clo" and alice.have_key == "no"):
+				alice.have_key = "yes"
 				print(enc_key)
+			elif (current_room == "clo" and alice.have_key == "yes"):
+				print(enc_found)
 		else:
 			print(enc_nothing)
 		
-	#Listing Inventory, Room Exits and Search
-	def exit_list(self):
-		print "You have" + str(inventory)
+	#Inventory, Room Exits and Search
+	def exit_list(self, alice):
+		if (alice.have_key == "yes" and alice.have_sword == "yes"):
+			print "A golden key is in your pocket, and you are holding a vorpal sword."
+		elif (alice.have_key == "yes" and alice.have_sword == "no"):
+			print "A golden key is in your pocket"
+		elif (alice.have_key == "no" and alice.have_sword == "yes"):
+			print "You are holding a vorpal sword."
 		if (self.n_exit == "TRUE"):
 			print "There is an exit to the (n)orth."
 		if (self.e_exit == "TRUE"):
@@ -101,22 +107,23 @@ den = Room("The Jabberwok's Den", den_desc, "nothing", "FALSE", "FALSE", "FALSE"
 #The Start of the Adventure
 print str(enc_start)
 
-#The Quest to Save the Mad Hatter
-while (jab == "alive"):
+#Game Start
+while (alice.jab == "alive"):
+	
 	#Determine Player Location
-	if (x_pos == 3 and y_pos == 1):
+	if (alice.x_pos == 3 and alice.y_pos == 1):
 		current_room = "ent"
-	elif (x_pos == 3 and y_pos == 2):
+	elif (alice.x_pos == 3 and alice.y_pos == 2):
 		current_room = "foy"
-	elif (x_pos == 3 and y_pos == 3):
+	elif (alice.x_pos == 3 and alice.y_pos == 3):
 		current_room = "clo"
-	elif (x_pos == 4 and y_pos == 2):
+	elif (alice.x_pos == 4 and alice.y_pos == 2):
 		current_room = "sit"
-	elif (x_pos == 4 and y_pos == 3):
+	elif (alice.x_pos == 4 and alice.y_pos == 3):
 		current_room = "gal"
-	elif (x_pos == 2 and y_pos == 2):
+	elif (alice.x_pos == 2 and alice.y_pos == 2):
 		current_room = "hal"
-	elif (x_pos == 1 and y_pos == 2):
+	elif (alice.x_pos == 1 and alice.y_pos == 2):
 		current_room = "den"
 	else:
 		print "You have somehow lost your way in the Tolgey Woods, despite being inside the Jabberwok's abode a moment ago.\n\n"
@@ -125,95 +132,109 @@ while (jab == "alive"):
 	#Conditionals for Each Room
 	#Ent
 	if (current_room == "ent"):
-		ent.room_enter()
-		ent.exit_list()
-		action = raw_input("What shall you do, Alice?")
+		ent.room_enter(alice)
+		ent.exit_list(alice)
+		action = raw_input("\nWhat shall you do, Alice?\n")
+		console.clear()
 		if (action == "h"):
-			ent.room_search()
+			ent.room_search(alice)
 		elif (action == "n"):
-			north()
+			alice.y_pos += 1
 		elif (action == "s"):
 			print str(enc_cannot)
 		else:
 			nope()
 	#Foy
 	elif (current_room == "foy"):
-		foy.room_enter()
-		foy.exit_list()
-		action = raw_input("What shall you do, Alice?")
+		foy.room_enter(alice)
+		foy.exit_list(alice)
+		action = raw_input("\nWhat shall you do, Alice?\n")
+		console.clear()
 		if (action == "h"):
-			foy.room_search()
+			foy.room_search(alice)
 		elif (action == "n"):
-			north()
+			alice.y_pos += 1
 		elif (action == "e"):
-			east()
+			alice.x_pos += 1
 		elif (action == "s"):
-			south()
+			alice.y_pos -= 1
 		elif (action == "w"):
-			west()
+			alice.x_pos -= 1
 		else:
 			nope()
 	#Clo
 	elif (current_room == "clo"):
-		clo.room_enter()
-		clo.exit_list()
-		action = raw_input("What shall you do, Alice?\n\n")
+		clo.room_enter(alice)
+		clo.exit_list(alice)
+		action = raw_input("\nWhat shall you do, Alice?\n")
+		console.clear()
 		if (action == "h"):
-			clo.room_search()
+			clo.room_search(alice)
 		elif (action == "s"):
-			south()
+			alice.y_pos -= 1
 		else:
 			nope()
 	#Sit
 	elif (current_room == "sit"):
-		sit.room_enter()
-		sit.exit_list()
-		action = raw_input("What shall you do, Alice?\n\n")
+		sit.room_enter(alice)
+		sit.exit_list(alice)
+		action = raw_input("\nWhat shall you do, Alice?\n")
+		console.clear()
 		if (action == "h"):
-			sit.room_search()
+			sit.room_search(alice)
 		elif (action == "n"):
-			north()
-		elif (action == "e"):
-			east()
+			alice.y_pos += 1
+		elif (action == "w"):
+			alice.x_pos -= 1
 		else:
 			nope()
 	#Gal
 	elif (current_room == "gal"):
-		gal.room_enter()
-		gal.exit_list()
-		action = raw_input("What shall you do, Alice?\n\n")
+		gal.room_enter(alice)
+		gal.exit_list(alice)
+		action = raw_input("\nWhat shall you do, Alice?\n")
+		console.clear()
 		if (action == "h"):
-			gal.room_search()
+			gal.room_search(alice)
 		elif (action == "s"):
-			south()
+			alice.y_pos -= 1
 		else:
 			nope()
 	#Hal
 	elif (current_room == "hal"):
-		hal.room_enter()
-		hal.exit_list()
-		action = raw_input("What shall you do, Alice?\n\n")
+		hal.room_enter(alice)
+		hal.exit_list(alice)
+		action = raw_input("\nWhat shall you do, Alice?\n")
+		console.clear()
 		if (action == "h"):
-			hal.room_search()
+			hal.room_search(alice)
 		elif (action == "e"):
-			east()
-		elif (action == "t" and "a golden key" in inventory == True):
+			alice.x_pos += 1
+		elif (action == "t" and alice.have_key == "yes"):
 			print str(enc_door)
-			east()
-		elif (action == "t" and "a golden key" in inventory == False):
+			alice.x_pos -= 1
+		elif (action == "t" and alice.have_key == "no"):
 			print str(enc_locked)
+		else:
+			nope()
 	#Den
 	elif (current_room == "den"):
-		den.room_enter()
-		den.exit_list()
+		console.clear()
+		den.room_enter(alice)
 		print str(enc_boss)
-		if ("a vorpal sword" in inventory == True):
-			jab == "dead"
+		if (alice.have_sword == "yes"):
+			alice.jab = "dead"
+			time.sleep(20)
+			console.clear()
+			break
 		else:
 			print "Uh oh...\n\n"
-		
+			time.sleep(20)
+			console.clear()
+			break
+
 #The Ending
-if (jab == "dead"):
+if (alice.jab == "dead"):
 	print str(enc_victory)
 else:
-	print str(defeat)
+	print str(enc_defeat)
